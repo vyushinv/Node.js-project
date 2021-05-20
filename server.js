0add308c3e8a93fed498a4feac92f4045f353a37
Vyacheslav Vyushin <76445562+vyushinv@users.noreply.github.com>
2021-05-20 09:17:44 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
-const express = require("express");
-const app = express();
-const server = require("http").Server(app);
-app.set("view engine", "ejs");
-
-const { ExpressPeerServer } = require("peer");
+const express = require("express")
+const app = express()
+const server = require("http").Server(app)
+app.set("view engine", "ejs")
+const { ExpressPeerServer } = require("peer")
 const peerServer = ExpressPeerServer(server, {
   debug: true,
   allow_discovery: true
 });
 
-app.use("/peerjs", peerServer);
-app.use(express.static("public"));
+app.use("/peerjs", peerServer)
+app.use(express.static("public"))
+
+const PORT = 9000
 
 app.get("/", (req, res) => {
-  res.render("client", {
+  res.render("main", {
   });
 });
 
-server.listen(process.env.PORT || 9000);
+server.listen(PORT);
 
 // peerServer events 
 peerServer.on('connection', (client) => {
